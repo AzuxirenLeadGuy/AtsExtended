01289f4a74ef42896598bf896e1422ae2ddbb1b0
Ashish <sam4lordjesus@outlook.com>
2024-01-01 18:06:59 +0530

    changed interface Iscene to abstract class

--- src/Framework/GameInit.cs
+++ src/Framework/GameInit.cs
@@ -2,10 +2,10 @@ namespace MyAtsGame.GameClass;
 /// <summary>The settings to initialize a custom defined game</summary>
 public struct GameInit<GameConstants>
 {
-    /// <summary>The custom game-specific constants</summary>
-    public GameConstants Constants;
-    /// <summary>The SFML Window settings for the game</summary>
-    public WindowSettings Settings;
-    /// <summary>The initial Scenes for the game at launch</summary>
-    public IScene<GameConstants> Foreground, Background;
+	/// <summary>The custom game-specific constants</summary>
+	public GameConstants Constants;
+	/// <summary>The SFML Window settings for the game</summary>
+	public WindowSettings Settings;
+	/// <summary>The initial Scenes for the game at launch</summary>
+	public Scene<GameConstants> Foreground, Background;
 }
--- src/Framework/IScene.cs
+++ src/Framework/IScene.cs
@@ -1,16 +1,18 @@
 using System;
+
+using SFML.Graphics;
 namespace MyAtsGame.GameClass;
 /// <summary>
 /// A game can be described as a set of Scenes that contains
 /// the focused, small portions of the game.
 /// </summary>
 /// <typeparam name="GameConstants">The game constants type on which the scene is based on</typeparam>
-public interface IScene<GameConstants>
+public abstract class Scene<GameConstants>
 {
     /// <summary>Initializes this scene; This method is called only at the start by the game object</summary>
     /// <param name="game">The game running the scene</param>
     /// <returns>returns the exception if any encountered; otherwise returns null</returns>
-    Exception? Load(SfmlGameClass<GameConstants> game) => null;
+    public virtual Exception? Load(SfmlGameClass<GameConstants> game) => null;
     /// <summary>
     /// Updates the given scene; also sends the next scene to progress without calling
     /// the Load() function on it. It is called automatically by the game object when needed.
@@ -19,7 +21,7 @@ public interface IScene<GameConstants>
     /// <param name="delta">The time difference from the previous frame</param>
     /// <param name="next">The reference to the next, uninitialized scene</param>
     /// <returns>returns the exception if any encountered; otherwise returns null</returns>
-    Exception? Update(SfmlGameClass<GameConstants> game, long delta, out IScene<GameConstants>? next)
+    public virtual Exception? Update(SfmlGameClass<GameConstants> game, long delta, out Scene<GameConstants>? next)
     {
         next = null;
         return null;
@@ -28,11 +30,12 @@ public interface IScene<GameConstants>
     /// Draws/Renders the game to the screen
     /// </summary>
     /// <param name="game">The game object being referenced</param>
+    /// <param name="window">The object that can render to the window</param>
     /// <param name="delta">The time difference from the previous frame</param>
     /// <returns>returns the exception if any encountered; otherwise returns null</returns>
-    Exception? Draw(SfmlGameClass<GameConstants> game, long delta) => null;
+    public virtual Exception? Draw(SfmlGameClass<GameConstants> game, in RenderTarget window, long delta) => null;
     /// <summary> Disposes any memory held by the scene.</summary>
     /// <param name="game">The game object being referenced</param>
     /// <returns>returns the exception if any encountered; otherwise returns null</returns>
-    Exception? Destroy(SfmlGameClass<GameConstants> game) => null;
+    public virtual Exception? Destroy(SfmlGameClass<GameConstants> game) => null;
 }
